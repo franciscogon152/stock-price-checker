7061c8691eb229bbc7a2418927862cab2070a4da
Glitch (illustrious-snowman) <none>
2018-12-18 14:52:33 +0000

    Updated with Glitch

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-**FreeCodeCamp**- Information Security and Quality Assurance
+**freeCodeCamp** - Information Security and Quality Assurance Project
 ------
 
-Project Stock Price Checker
+**Stock Price Checker**
 
-1) SET NODE_ENV to `test` without quotes and set DB to your mongo connection string
+1) SET NODE_ENV to `test` without quotes and set DATABASE to your Mongo connection string
 2) Complete the project in `routes/api.js` or by creating a handler/controller
 3) You will add any security features to `server.js`
 4) You will create all of the functional tests in `tests/2_functional-tests.js`
-
-
--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,106 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var request = require('request');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+module.exports = function(app) {
 
-module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
+  app.route('/api/stock-prices').get(function(req, res) {
+    if (req.query.stock === undefined) {
+      return res.json({ error: 'stock is required' });
+    }
     
+    let stock = req.query.stock;
+    let like = (req.query.like !== undefined && req.query.like === 'true' ? true : false);
+    
+    if (Array.isArray(stock)) {
+      if (stock.length > 2) {
+        return res.json({ error: 'only 1 or 2 stock is supported' });
+      }
+    } else {
+      stock = [stock];
+    }
+    
+    MongoClient.connect(process.env.DATABASE, { useNewUrlParser: true }, function(err, db) {
+      if (err) {
+        // console.log('Database error: ' + err);
+        return res.json({ error: 'error' });
+      } else {
+        stock[0] = stock[0].toUpperCase();
+        
+        let updateObj = {
+          $setOnInsert: {
+            stock: stock[0]
+            // likes: req['ip'] || []
+          }
+        };
+        
+        if (like) {
+          updateObj['$addToSet'] = {
+            likes: req['ip']
+          };
+        }
+        
+        db.db().collection('stock').findOneAndUpdate(
+          {
+            stock: stock[0]
+          },
+          updateObj,
+          { upsert: true, returnOriginal: false }, // Insert object if not found, Return updated object after modify
+          function(error, result) {
+            let likes = (result.value.likes !== undefined ? result.value.likes.length : 0);
+            
+            request('https://api.iextrading.com/1.0/stock/'+stock[0]+'/price', function(error, response, body) {
+              if (stock[1] === undefined) {
+                return res.json({ stockData: { stock: stock[0], price: body, likes: likes } });
+              } else {
+                let stock_result = [];
+                stock_result.push({ stock: stock[0], price: body, rel_likes: likes });
+
+                stock[1] = stock[1].toUpperCase();
+                
+                updateObj = {
+                  $setOnInsert: {
+                    stock: stock[1]
+                    // likes: req['ip'] || []
+                  }
+                };
+
+                if (like) {
+                  updateObj['$addToSet'] = {
+                    likes: req['ip']
+                  };
+                }
+
+                db.db().collection('stock').findOneAndUpdate(
+                  {
+                    stock: stock[1]
+                  },
+                  updateObj,
+                  { upsert: true, returnOriginal: false }, // Insert object if not found, Return updated object after modify
+                  function(error, result2) {
+                    likes = (result2.value.likes !== undefined ? result2.value.likes.length : 0);
+
+                    request('https://api.iextrading.com/1.0/stock/'+stock[1]+'/price', function(error, response, body2) {
+                      stock_result.push({ stock: stock[1], price: body2, rel_likes: likes });
+                      
+                      let rel_likes1 = stock_result[0]['rel_likes'] - stock_result[1]['rel_likes'];
+                      let rel_likes2 = stock_result[1]['rel_likes'] - stock_result[0]['rel_likes'];
+                      
+                      stock_result[0]['rel_likes'] = rel_likes1;
+                      stock_result[1]['rel_likes'] = rel_likes2;
+
+                      return res.json({ stockData: stock_result });
+                    });
+                  }
+                );
+              }
+            });
+          }
+        );
+      }
+    });
+  });
+  
 };
