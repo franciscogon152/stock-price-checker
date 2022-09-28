0bd49d8bdd2289914648a4156d1774d506afa174
Adam777 <Adam777-5867@outlook.com>
2022-09-28 12:38:56 +0000

    Update

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 **Stock Price Checker**
 
-1) SET NODE_ENV to `test` without quotes and set DATABASE to your Mongo connection string
+1) SET NODE_ENV to `test` without quotes and set MONGO_URI to your Mongo connection string
 2) Complete the project in `routes/api.js` or by creating a handler/controller
 3) You will add any security features to `server.js`
 4) You will create all of the functional tests in `tests/2_functional-tests.js`
--- routes/api.js
+++ routes/api.js
@@ -18,10 +18,10 @@ module.exports = function(app) {
     if (req.query.stock === undefined || req.query.stock === '') {
       return res.json({ error: 'stock is required' });
     }
-    
+
     let stock = req.query.stock;
     let like = (req.query.like !== undefined && req.query.like === 'true' ? true : false);
-    
+
     if (Array.isArray(stock)) {
       if (stock.length > 2) {
         return res.json({ error: 'only 1 or 2 stock is supported' });
@@ -29,27 +29,27 @@ module.exports = function(app) {
     } else {
       stock = [stock];
     }
-    
-    MongoClient.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+
+    MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
       if (err) {
         // console.log('Database error: ' + err);
         return res.json({ error: 'error' });
       } else {
         stock[0] = stock[0].toUpperCase();
-        
+
         let updateObj = {
           $setOnInsert: {
             stock: stock[0]
             // likes: req['ip'] || []
           }
         };
-        
+
         if (like) {
           updateObj['$addToSet'] = {
             likes: req['ip']
           };
         }
-        
+
         db.db().collection('stock').findOneAndUpdate(
           {
             stock: stock[0]
@@ -58,7 +58,7 @@ module.exports = function(app) {
           { upsert: true, returnOriginal: false }, // Insert object if not found, Return updated object after modify
           function(error, result) {
             let likes = (result.value.likes !== undefined ? result.value.likes.length : 0);
-            
+
             request('https://cloud.iexapis.com/stable/stock/'+stock[0].toLowerCase()+'/quote/latestPrice?token='+process.env.STOCK_API_TOKEN, function(error, response, body) {
               if (stock[1] === undefined) {
                 return res.json({ stockData: { stock: stock[0], price: body, likes: likes } });
@@ -67,7 +67,7 @@ module.exports = function(app) {
                 stock_result.push({ stock: stock[0], price: body, rel_likes: likes });
 
                 stock[1] = stock[1].toUpperCase();
-                
+
                 updateObj = {
                   $setOnInsert: {
                     stock: stock[1]
@@ -92,10 +92,10 @@ module.exports = function(app) {
 
                     request('https://cloud.iexapis.com/stable/stock/'+stock[1].toLowerCase()+'/quote/latestPrice?token='+process.env.STOCK_API_TOKEN, function(error, response, body2) {
                       stock_result.push({ stock: stock[1], price: body2, rel_likes: likes });
-                      
+
                       let rel_likes1 = stock_result[0]['rel_likes'] - stock_result[1]['rel_likes'];
                       let rel_likes2 = stock_result[1]['rel_likes'] - stock_result[0]['rel_likes'];
-                      
+
                       stock_result[0]['rel_likes'] = rel_likes1;
                       stock_result[1]['rel_likes'] = rel_likes2;
 
@@ -110,5 +110,5 @@ module.exports = function(app) {
       }
     });
   });
-  
+
 };
